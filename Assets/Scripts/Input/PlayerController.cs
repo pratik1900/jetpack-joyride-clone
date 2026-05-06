12732104a6f8c18281fd442986a1e9b4f285cdbb
Pratik Bose <pratik.bose@lennar.com>
2026-05-06 21:28:31 +0530

    Added scrolling bckground, vertical claamping for player

--- Assets/Scripts/Input/PlayerController.cs
+++ Assets/Scripts/Input/PlayerController.cs
@@ -10,6 +10,11 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float thrustSpeed = 5f;
     // [SerializeField] private float fallSpeed = 8f;
 
+    // Upper bound of the playable area
+    private float roofY = 4.2f;
+    // Lower bound of the playable area
+    private float groundY = -3.7f;
+
     private InputAction thrustAction;
 
     void Awake()
@@ -26,6 +31,14 @@ public class PlayerController : MonoBehaviour
         {
             playerRb.AddForce(Vector2.up * thrustSpeed, ForceMode2D.Force);
         }
+
+        // Clamp vertical position to playable area bounds
+        float clampedY = Mathf.Clamp(transform.position.y, groundY, roofY);
+        transform.position = new Vector3(transform.position.x, clampedY, transform.position.z);
+
+        // Kill upward velocity if at roof
+        if (transform.position.y >= roofY && playerRb.linearVelocity.y > 0)
+            playerRb.linearVelocity = new Vector2(playerRb.linearVelocity.x, 0);
     }
 
     private void OnEnable()
