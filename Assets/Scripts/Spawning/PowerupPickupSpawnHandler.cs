11d899dbb65d29966d1a38bca2acfdd63a474eb6
Pratik Bose <bose.pratik1900@gmail.com>
2026-08-12 15:41:37 +0530

    added main menu

--- Assets/Scripts/Spawning/PowerupPickupSpawnHandler.cs
+++ Assets/Scripts/Spawning/PowerupPickupSpawnHandler.cs
@@ -2,8 +2,6 @@ using UnityEngine;
 
 public class PowerupPickupSpawnHandler : SpawnHandler
 {
-    // [SerializeField] private PowerupEffect[] availablePowerupEffects;
-
     public override ObjectTags ObjectTag => ObjectTags.PowerupPickup;
 
     public override void Spawn(Vector3 position)
@@ -13,16 +11,5 @@ public class PowerupPickupSpawnHandler : SpawnHandler
             position,
             Quaternion.identity
         );
-
-        // PowerupPickup pickup = spawnedPickupObject?.GetComponent<PowerupPickup>();
-
-        // PowerupEffect chosenPowerup = GetRandomPowerupEffect();
-
-        // pickup.Initialize(chosenPowerup);
     }
-
-    // private PowerupEffect GetRandomPowerupEffect()
-    // {
-    //     return availablePowerupEffects[Random.Range(0, availablePowerupEffects.Length)];
-    // }
 }
